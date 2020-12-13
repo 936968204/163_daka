551870ba27b2202e241ef1d4426d209947bdb40d
936968204 <75925804+936968204@users.noreply.github.com>
2020-12-13 15:03:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 用户名和密码
-const phone = '';
-const password = '';
+const phone = '18874621768';
+const password = '120435688';
 
 const rp = require('request-promise');
 const tough = require('tough-cookie');
